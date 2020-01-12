453aad25a75cfb122ed557a27341c1b6745992b4
Nicolas Kepper <nicolas.kepper@gmail.com>
2020-01-12 11:04:13 +0100

    Initial Commit

---
+++ FlightCompensatioin/src/main/java/com/divirad/flightcompensation/FlightCompensatioin/App.java
@@ -0,0 +1,15 @@
+package com.divirad.flightcompensation.FlightCompensatioin;
+
+import org.opensky.api.OpenSkyApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        OpenSkyApi api = new OpenSkyApi("abc", "def");
+    }
+}
