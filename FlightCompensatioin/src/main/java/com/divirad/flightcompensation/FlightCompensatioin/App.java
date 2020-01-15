b27fdc0cacf565106ca3256baf0bbffdfd454ef6
h4098099 <h4098099@FSTR024.fs.lan>
2020-01-15 15:06:41 +0100

    Setup Database infrastructure

--- FlightCompensatioin/src/main/java/com/divirad/flightcompensation/FlightCompensatioin/App.java
+++ FlightCompensatioin/src/main/java/com/divirad/flightcompensation/FlightCompensatioin/App.java
@@ -1,15 +1,9 @@
 package com.divirad.flightcompensation.FlightCompensatioin;
 
-import org.opensky.api.OpenSkyApi;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        OpenSkyApi api = new OpenSkyApi("abc", "def");
+public class App {
+    
+	
+	public static void main(String[] args) {
+		
     }
 }
