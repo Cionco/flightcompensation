ed651967862a4c461ee0719b63073c89b689c12c
Nicolas Kepper <nicolas.kepper@gmail.com>
2020-01-17 21:31:32 +0100

    Make API Request work

--- FlightCompensationMonolith/src/main/java/com/divirad/flightcompensation/monolith/data/api/FlightDataLoader.java
+++ FlightCompensationMonolith/src/main/java/com/divirad/flightcompensation/monolith/data/api/FlightDataLoader.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
 
 public class FlightDataLoader {
 	
-	private static final String HOSTNAME = "http://api.aviationstack.com/v1/";
+	private static final String HOSTNAME = "https://api.aviationstack.com/v1/";
 	private static final String ACCESS_KEY = "fd5dc26aa131c9e7f60f5ce54cf459ce";
 
 	public static FlightDataLoader instance;
@@ -44,6 +44,8 @@ System.out.println(url);
 		    URL obj = new URL(url);
 		    HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 		 
+		    conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+		    
 		    conn.setDoOutput(false);
 		 
 		    conn.setRequestMethod("GET");
