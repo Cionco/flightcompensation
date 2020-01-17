ebdd4de370ef7997f4fb4f8649d7e140db3f3c96
Nicolas Kepper <nicolas@divirad.com>
2020-01-17 18:19:59 +0100

    Setup API Loader infrastructure

--- FlightCompensationMonolith/src/main/java/com/divirad/flightcompensation/monolith/App.java
+++ FlightCompensationMonolith/src/main/java/com/divirad/flightcompensation/monolith/App.java
@@ -4,11 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.json.JSONObject;
 
 import com.divirad.flightcompensation.monolith.data.Flight;
+import com.divirad.flightcompensation.monolith.data.api.FlightDataLoader;
+import com.divirad.flightcompensation.monolith.data.api.FlightDataLoader.Constraint;
 import com.divirad.flightcompensation.monolith.data.api.Parser;
 import com.divirad.flightcompensation.monolith.data.database.FlightDao;
 
@@ -19,17 +25,21 @@ public class App {
 	
 	public static void main(String[] args) {
 		ArrayList<Flight> flights = null;
+		FlightDataLoader fdl = FlightDataLoader.getInstance(	new Constraint("flight_date", () -> LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+															,	new Constraint("dep_iata", () -> "fra"));
 		
-		File f = new File("C:\\Users\\h4098099\\Desktop\\xamplejson.json");
-		try {
-			String content = new String(Files.readAllBytes(Paths.get(f.toURI())), "UTF-8");
-			JSONObject o = new JSONObject(content);
-			flights = Parser.getInstance().parseFlights(o.getJSONArray("data"));
+		JSONObject o = fdl.getApiFlightData();
+		
+//		File f = new File("C:\\Users\\h4098099\\Desktop\\xamplejson.json");
+//		try {
+//			String content = new String(Files.readAllBytes(Paths.get(f.toURI())), "UTF-8");
+//			JSONObject o = new JSONObject(content);
+		flights = Parser.getInstance().parseFlights(o.getJSONArray("data"));
 			
-		} catch(IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
+//		} catch(IOException e) {
+//			e.printStackTrace();
+//			System.exit(-1);
+//		}
 		
 		FlightDao.instance.storeFlights(flights);
     }
--- FlightCompensationMonolith/src/main/java/com/divirad/flightcompensation/monolith/data/api/FlightDataLoader.java
+++ FlightCompensationMonolith/src/main/java/com/divirad/flightcompensation/monolith/data/api/FlightDataLoader.java
@@ -1,14 +1,72 @@
 package com.divirad.flightcompensation.monolith.data.api;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.StringJoiner;
+
+import org.json.JSONObject;
+
 public class FlightDataLoader {
+	
+	private static final String HOSTNAME = "http://api.aviationstack.com/v1/";
+	private static final String ACCESS_KEY = "fd5dc26aa131c9e7f60f5ce54cf459ce";
 
 	public static FlightDataLoader instance;
 	
-	private FlightDataLoader() {}
+	public interface IDetValue {
+		public String value();
+	}
 	
-	public static FlightDataLoader getInstance() {
-		if(instance == null) instance = new FlightDataLoader();
+	private ArrayList<Constraint> constraints = new ArrayList<>();
+	
+	private FlightDataLoader(Constraint... constraints) {
+		for(Constraint c : constraints)
+			this.constraints.add(c);
+	}
+	
+	public static FlightDataLoader getInstance(Constraint... constraints) {
+		if(instance == null) instance = new FlightDataLoader(constraints);
 		return instance;
+	}
+	
+	public JSONObject getApiFlightData() {
+		StringJoiner filter = new StringJoiner("&", "?", "");
+		filter.add("access_key=" + ACCESS_KEY);
+		for(Constraint c : constraints)
+			filter.add(c.key + "=" + c.value.value());
+		String url = HOSTNAME + "flights" + filter;
+System.out.println(url);
+		String output = "";
+		try {  
+		    URL obj = new URL(url);
+		    HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+		 
+		    conn.setDoOutput(false);
+		 
+		    conn.setRequestMethod("GET");
+		    
+		    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			for (String line; (line = reader.readLine()) != null;) {
+			        output += line;
+			}
+			reader.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new JSONObject(output);
+	}
+	
+	public static class Constraint {
+		public String key;
+		public IDetValue value;
+		
+		public Constraint(String key, IDetValue value) {
+			this.key = key;
+			this.value = value;
+		}
 	}
 	
 	
